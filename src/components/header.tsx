75180f05abf0ad3e792c7236e008ebaaa7b0c078
Matheus Kuaye Muramoto <matheusmuramoto@gmail.com>
2024-03-06 19:54:54 -0300

    change name and photo in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -68,11 +68,11 @@ export function Header() {
 
             <div className="flex items-center gap-3">
                 <div className="flex flex-col items-end gap-0.5">
-                    <span className="text-sm font-medium">Diego Fernandes</span>
-                    <span className="text-xs text-zinc-400">diego@nivo.video</span>
+                    <span className="text-sm font-medium">Matheus Muramoto</span>
+                    <span className="text-xs text-zinc-400">matheus@teste.com</span>
                 </div>
                 <img
-                    src="https://github.com/diego3g.png"
+                    src="https://github.com/mkuaye.png"
                     className="size-8 rounded-full"
                     alt=""
                 />
